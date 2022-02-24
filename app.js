80d80cfddb7d9437b36b4f3d8c0569a9d592bf23
ugbeadie <ugbeadie3@gmail.com>
2022-02-24 01:28:43 +0100

    loopstudio

---
+++ app.js
@@ -0,0 +1,22 @@
+const burger = document.getElementsByClassName('burger')[0];
+const navUl = document.getElementsByClassName('nav-items')[0];
+const navMenu = document.querySelector('nav')
+const close = document.querySelector('close')
+
+
+burger.addEventListener('click', () => {
+    navUl.classList.toggle('active');
+    navMenu.classList.toggle('active')
+    burger.classList.toggle('active');
+    
+    // document.body.classList.toggle('hidden')
+})
+
+// close.addEventListener('click', () => {
+//     navUl.classList.toggle('active');
+//     navMenu.classList.toggle('navmenu')
+//     close.classList.toggle('active');
+
+//     // document.body.classList.toggle('hidden')
+// })
+
